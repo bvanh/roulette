3b071a8875e1471f4290c483f5b2a90397839ecd
bvanh <48560384+bvanh@users.noreply.github.com>
2020-07-20 17:09:35 +0700

    demo cookie2

--- src/utils/cookieService.js
+++ src/utils/cookieService.js
@@ -1,8 +1,8 @@
 import Cookies from 'js-cookie'
 const cookieService = {
     setToken: (token, accessToken) => {
-        Cookies.set("tokenRoulette", token, { expires: 60, secure: true });
-        Cookies.set("accessTokenRoulette", accessToken, { secure: true });
+        Cookies.set("tokenRoulette", token, { expires: 60, secure: true, HttpOnly: true });
+        Cookies.set("accessTokenRoulette", accessToken, { secure: true, HttpOnly: true });
     },
     getAccessToken: () => {
         return Cookies.get('accessTokenRoulette');
